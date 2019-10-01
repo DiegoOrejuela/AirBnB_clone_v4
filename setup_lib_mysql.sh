3cee56b9cb6c1a3f5e81deb45daeb0aa1dd07dbf
DiegoOrejuela <dfrodriguezor@gmail.com>
2019-10-01 05:55:51 +0000

    [feature] 2. Select some Amenities to be comfortable!

---
+++ setup_lib_mysql.sh
@@ -0,0 +1,9 @@
+#Configurate several libraries of Mysql for Pyhton
+# 1. MySQLdb
+sudo apt-get install python3-dev
+sudo apt-get -y install libmysqlclient-dev
+sudo apt-get install zlib1g-dev
+sudo pip3 install mysqlclient==1.3.10
+
+# 2. SQLAlchemy
+pip3 install SQLAlchemy==1.2.5
