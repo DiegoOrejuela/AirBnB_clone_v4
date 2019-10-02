42db42e5222311cb87c2f004a1ce1963eb3161f8
xica369 <761@holbertonschool.com>
2019-10-02 01:39:12 +0000

    T3: update file 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -2,6 +2,13 @@ $( document ).ready(function() {
   myIds = []
   myNames = []
   let flag;
+  $.getJSON('http://0.0.0.0:5001/api/v1/status/', function( data ) {
+    if (data.status === 'OK') {
+      $(DIV#api_status).addClass(available);
+    } else {
+      $(DIV#api_status).removeClass(available);
+    }
+  });
   $("UL.popover INPUT").on( 'change', function() {
     if( $(this).is(':checked') ) {
         myIds.push($(this).attr('data-id'));
